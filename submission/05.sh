7c57f589f667cd6976d70b1d3ffcc7ee2a57d7bb
Zairo <edwinmoyi8@email.com>
2026-04-14 01:21:34 +0300

    05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,6 +1,6 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-
-# printf "%x\n" 150 - 96 - 000096 - 960000
-
-printf "%s" "03960000b2752102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277ac"
+# Public Key Hash: 1e51fcdc14be9a148bb0aaec9197eb47c83776fb
+# printf "%x\n" 150 - 96 - 0296
+# 0296b27576a914<pubKeyHash>88ac
+printf "%s" "0296b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
