32bb0e2f48902f77d6f0f04b3f165a5bd1c8766d
Zairo <edwinmoyi8@email.com>
2026-04-14 02:07:53 +0300

    08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -2,4 +2,4 @@
 # Time-based CSV uses 512-second units with the type flag (bit 22) set
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-printf "%s" "674a4000b2752102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277ac"
+printf "%s" "03a77640b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
